3f482796c6003ab6d437716e2966c14ea6942404
springcat <329839705@qq.com>
2016-02-18 14:17:41 +0800

    （1）添加json中null自动转化成空字符串的功能 （2）添加mybatis生成器生成cache的功能

---
+++ support/maven-archetype/src/main/resources/archetype-resources/src/main/java/rest/ObjectMapper.java
@@ -0,0 +1,31 @@
+package org.springcat.sample.rest;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import java.io.IOException;
+
+public class ObjectMapper extends ObjectMapper
+{
+
+    public ObjectMapper()
+    {
+        super();
+
+        // 空值处理为空串
+        this.getSerializerProvider().setNullValueSerializer(new JsonSerializer<Object>()
+        {
+            @Override
+            public void serialize(
+                    Object value,
+                    JsonGenerator jg,
+                    SerializerProvider sp) throws IOException, JsonProcessingException
+            {
+                jg.writeString("");
+            }
+        });
+
+    }
+}
